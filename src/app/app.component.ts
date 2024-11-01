03caeda34fd62c9c40ed265bcbbcf54edeb3f08f
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-11-01 05:53:55 -0500

    elegirnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from "./inicio/inicio.component";
-
+import { PorQueElegirnosComponent } from './por-que-elegirnos/por-que-elegirnos.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent],
+  imports: [RouterOutlet, InicioComponent, PorQueElegirnosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
